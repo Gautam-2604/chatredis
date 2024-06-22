39f075bb7ba7375381da5faae60d0d722d3e5a30
Gautam-2604 <gaurisariag@gmail.com>
2024-06-22 16:32:35 +0530

    login added using nextAuth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import ToastProvider from "@/providers/toastProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+        <ToastProvider>
+        {children}
+        </ToastProvider>
+        </body>
     </html>
   );
 }
