d2bf6623c30eeda22ffc72b7d188c51648581538
Gautam-2604 <gaurisariag@gmail.com>
2024-06-23 10:40:01 +0530

    add friend functionality added

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -8,9 +8,18 @@ export async function POST(req:Request){
         const {email: emailToAdd} = addFriendValidator.parse(body.email)
         const RestResponse = await fetch(`${process.env.UPSTASH_REDIS_REST_UR}/get/user:email${emailToAdd}`,{headers:{Authorization:`Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`}, cache:'no-store'})
 
-        const data = (await RestResponse.json()) as {result: string}
+        const data = (await RestResponse.json()) as {result: string|null}
         const idToAdd = data.result
-        const session = getServerSession(authOptions)
+        if(!idToAdd){
+            return new Response('This person does not exist')
+        }
+        const session = await getServerSession(authOptions)
+        if(!session){
+            return new Response('Unauthorised',{status:401})
+        }
+        if(idToAdd===session.user.id){
+            return new Response('You cannot add yourself you goof!!',{status:400})
+        }
     } catch (error) {
         
     }
